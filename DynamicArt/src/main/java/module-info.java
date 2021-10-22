d5da544481b3215e1ba39880f63fdd7c5f6f42ba
yasirn08 <50393848+yasirn08@users.noreply.github.com>
2021-10-22 22:17:56 +0600

    Delete DynamicArt directory

--- DynamicArt/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.dynamicart {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.dynamicart to javafx.fxml;
-    exports com.example.dynamicart;
-}
